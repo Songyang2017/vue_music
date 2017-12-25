c08498fd9b344e3f473838ddf1cffe9cea858ad4
Songyang2017 <1546302993@qq.com>
2017-12-25 23:29:56 +0800

    0.0.0

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style scoped lang="stylus">
+@import "~common/stylus/variable"
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: $color-theme;
+  margin-top: 60px;
+}
+</style>
