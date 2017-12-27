4f0aedb87c9df0333980fd0a64baca5e22d33674
Songyang2017 <1546302993@qq.com>
2017-12-28 00:01:17 +0800

    0.0.1.2 完成header和tab

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <m-header></m-header>
+    <tab></tab>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+import MHeader from 'components/m-header/m-header'
+import Tab from 'components/tab/tab'
+
 export default {
-  name: 'app'
+  components: {
+    MHeader,
+    Tab
+  }
 }
 </script>
 
 <style scoped lang="stylus">
-@import "~common/stylus/variable"
 
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: $color-theme;
-  margin-top: 60px;
-}
 </style>
