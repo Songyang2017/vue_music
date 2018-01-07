0e6016866177f8bf67f62339d1c98d36ff550f2c
Songyang2017 <1546302993@qq.com>
2018-01-07 23:06:00 +0800

    0.0.1.8 2018-01-07搬家后继续进行中，提交时项目没爬起来，下次继续

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,14 +1,52 @@
 <template>
-  <div>歌手页面</div>
+  <div class="singer"></div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {
+  import {getSingerList} from 'api/singer'
+  import {ERR_OK} from 'api/config'
 
+  const HOT_NAME = '热门'
+
+  export default {
+    data() {
+      return {
+        singers: []
+      }
+    },
+    created() {
+      this._getSingerList()
+    },
+    methods: {
+      _getSingerList() {
+        getSingerList().then((res) => {
+          if (res.code === ERR_OK) {
+            this.singers = res.data.list
+            console.log(this.singers)
+          }
+        })
+      },
+      _normallizeSinger(list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+        list.forEach((item, index) => {
+
+        })
+      }
+    }
   }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="stylus">
-
+  .singer {
+    position: absolute;
+    top: 88px;
+    bottom: 0;
+    width: 100%;
+  }
 </style>
